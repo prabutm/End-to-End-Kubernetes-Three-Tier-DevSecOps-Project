56929d25ab3f451b48b8eea4ace1881cdeb07e69
prabutm <98154930+prabutm@users.noreply.github.com>
2026-07-07 18:58:24 +0100

    Refactor Trivy installation in tools-install.sh
    Updated Trivy installation method to use GPG keyring.

--- Jenkins-Server-TF/tools-install.sh
+++ Jenkins-Server-TF/tools-install.sh
@@ -65,11 +65,10 @@ sudo apt install terraform -y
 # Installing Trivy
 #!/bin/bash
 sudo apt-get install wget apt-transport-https gnupg lsb-release -y
-wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
-echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
-sudo apt update
-sudo apt install trivy -y
-
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo gpg --dearmor -o /usr/share/keyrings/trivy.gpg
+echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb jammy main" | sudo tee /etc/apt/sources.list.d/trivy.list
+sudo apt-get update
+sudo apt-get install trivy -y
 
 # Intalling Helm
 #! /bin/bash
